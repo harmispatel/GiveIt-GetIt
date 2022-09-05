0614436005f285a4e53e72ba8d5f5d1617791c55
Kartik Panchal <kartikp@harmistechnology.com>
2022-09-05 14:31:54 +0530

    changes route name

--- routes/web.php
+++ routes/web.php
@@ -39,46 +39,20 @@ use Illuminate\Support\Facades\Auth;
 // Admin Route
 
 Route::group(['middleware' => ['guest']], function () {
-<<<<<<< HEAD
-
-    Route::get('/admin/login',[loginController::class,'index'])->name('loginform');
-    Route::post('/admin/login',[loginController::class,'check'])->name('login');
-    Route::get('/admin/registration',[RegistrationController::class,'index'])->name('registrationForm');
-    Route::post('/admin/registration',[RegistrationController::class,'store'])->name('registration');
-    
-
-    //Forgot Password
-
-    Route::get('/forgotPassword', [AdminForgotPasswordController::class,'index'])->name('forgotPassword');
-    Route::post('/forgotPassword', [AdminForgotPasswordController::class,'submitForm'])->name('submitForm');
-    Route::get('/resetPassword/{token}', [AdminForgotPasswordController::class,'resetPasswordForm'])->name('getResetPassword');
-    Route::post('/postResetPassword/{token}', [AdminForgotPasswordController::class,'submitResetPasswordForm'])->name('postResetPassword');
-
-=======
     Route::get('/loginform',[loginController::class,'index'])->name('loginform');
     Route::post('/login', [loginController::class,'check'])->name('login');
     Route::get('/registration', [RegistrationController::class,'index'])->name('registrationForm');
     Route::post('/registration', [RegistrationController::class,'store'])->name('registration');
     
     //Forgot Password
->>>>>>> 7e6909c4cbee82c2c37de7f32ce71b8be93595b3
     Route::get('/forgotPassword',[AdminForgotPasswordController::class,'index'])->name('forgotPassword');
     Route::post('/forgotPassword',[AdminForgotPasswordController::class,'submitForm'])->name('submitForm');
     Route::get('/resetPwd/{token}',[AdminForgotPasswordController::class,'resetPasswordForm'])->name('resetpassword');
     Route::post('/postResetPassword',[AdminForgotPasswordController::class,'submitResetPasswordForm'])->name('postResetPassword');
 
-<<<<<<< HEAD
-
 });
 
 Route::group(['middleware' => ['auth']], function () {
-
-    Route::view('/admin/dashboard','welcome');
-=======
-});
-
-Route::group(['middleware' => ['auth']], function () {
->>>>>>> 7e6909c4cbee82c2c37de7f32ce71b8be93595b3
     
     // Logout Route
     
@@ -92,15 +66,10 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('/requirement', RequirementController::class);
     
     // Filter Route
-<<<<<<< HEAD
-    Route::post('/filter',[RequirementController::class,'changeStatus']);
-
-=======
     Route::post('/filterStatus', [RequirementController::class,'changeStatus']);
     Route::post('/filterIsActive', [RequirementController::class,'changeIsActive']);
     Route::post('/search', [RequirementController::class,'searching']);
     
->>>>>>> 7e6909c4cbee82c2c37de7f32ce71b8be93595b3
     // Profile Route
     Route::resource('/adminProfile', AdminProfileController::class);
 });
@@ -109,59 +78,6 @@ Route::group(['middleware' => ['auth']], function () {
 
 
 
-<<<<<<< HEAD
-
-
-// Front-end Route
-
-
-Route::view("/welcome", 'fronted.index');
-Route::view("/aboutus", 'fronted.about');
-
-
-// login & Rqgister user
-Route::get("/register",[RegisterController::class, 'show'])->name('register');
-Route::get("/userlogin", [UserController::class,'home'])->name('userlogin')->middleware('guest');
-Route::get('account/verify/{token}', [RegisterController::class, 'verifyAccount'])->name('user.verify');
-Route::post("/Register-insertdata", [RegisterController::class, 'store'])->name('Regitser.insertdata');
-Route::post("/userget", [UserController::class,'usercheck'])->name('useget')->middleware('guest');
-
-
-// Forgot password
-Route::get('/forget-password', [ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
-Route::post('/forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post');
-Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
-Route::post('/reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
-
-// Giveit & Getit page
-Route::get('/giveit', [GiveitController::class,'index'])->name('giveit');
-Route::get('/getit', [GetitController::class,'index'])->name('getit');
-Route::get("/giveview-detail/{id}", [GiveitController::class,'show'])->name('giveviewdetail');
-Route::get("/getview-detail/{id}", [GetitController::class,'show'])->name('getitview');
-
-
-Route::group(['middleware' => ['auth']], function () {
-    
-// Logout User
-Route::post("/logout", [UserController::class,'userLogout'])->name('userlogout');
-
-// Requirement 
-Route::get("/insertform", [RequirementController::class,'showinsert'])->name('insertform');
-Route::get("/required-data", [RequirementController::class,'display'])->name('required');
-Route::get("/Required-form", [AddRequirementController::class,'index'])->name('addform');
-Route::post("/Required/insert-data", [AddRequirementController::class,'storeRequirement'])->name('insertdata');
-Route::get("/Required/edit/{id}", [AddRequirementController::class,'edit'])->name('edit');
-Route::delete("/delete-Requirement/{id}", [AddRequirementController::class,'destroy'])->name('deleteRequirement');
-Route::post("/Required/update/{id}", [AddRequirementController::class,'update'])->name('update');
-Route::post('/add-to-favorite', [FavoriteController::class,'store'])->name('add-to-favorite');
-Route::get('/display-favorites', [FavoriteController::class,'show'])->name('displayfavorites');
-Route::delete("/delete/{id}", [FavoriteController::class,'delete'])->name('delete');
-Route::get("/editprofile", [UserProfileController::class,'edit'])->name('editprofile');
-Route::post("/User/update-profile", [UserProfileController::class,'update'])->name('userupdateprofile');
-Route::post("/User/update-password", [UserProfileController::class,'password'])->name('updatepassword');
-});
-
-=======
 // Front-end Route
 
 
@@ -213,4 +129,3 @@ Route::post("/User/update-password", [UserProfileController::class,'password'])-
 
 
     
->>>>>>> 7e6909c4cbee82c2c37de7f32ce71b8be93595b3
